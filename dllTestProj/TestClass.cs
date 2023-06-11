cdfd15bb51ad999ee82f75958b37b0af5ce8bd82
cwl625 <cwlee@dowoomets.co.kr>
2023-06-12 02:30:36 +0900

    초기 커밋.

---
+++ dllTestProj/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dllTestProj
+{
+    public class TestClass
+    {
+
+        public string AddString(string str)
+        {
+            str = str + "_dll_Test_Return";
+
+            return str;
+        }
+
+
+    }
+}
